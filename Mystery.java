17d5235314cfa584c2b6c54461f27508251d5a71
Irene <35508977+guzman579@users.noreply.github.com>
2019-02-05 17:02:25 -0800

    Create Mystery.java

---
+++ Mystery.java
@@ -0,0 +1,15 @@
+// Exercise 4.16: Mystery.java
+public class Mystery 
+{
+   public static void main(String[] args)
+
+   {
+      int count = 1;
+
+      while (count <= 10) 
+      {
+         System.out.println(count % 2 == 1 ? "****" : "++++++++");
+         ++count;
+      } 
+   }
+} // end class Mystery
